f38492ac32be0413ae2e2ee5a36f160069a9485e
💪🏻 Luis Gallegos 🏋🏻‍♂️ <lgallegos@ohksystems.com>
2018-06-14 16:43:07 -0500

    se agrega directiva para mayúsuculas

--- home/directive/directives_home.js
+++ home/directive/directives_home.js
@@ -7,3 +7,26 @@ expediente.directive('dirMenu', [function () {
         templateUrl: '../views/menu.html'
     };
 }]);
+
+
+
+/* directivas de sobre los elementos */
+
+expediente.directive('capitalize', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, modelCtrl) {
+            var capitalize = function (inputValue) {
+                if (inputValue == undefined) inputValue = '';
+                var capitalized = inputValue.toUpperCase();
+                if (capitalized !== inputValue) {
+                    modelCtrl.$setViewValue(capitalized);
+                    modelCtrl.$render();
+                }
+                return capitalized;
+            }
+            modelCtrl.$parsers.push(capitalize);
+            capitalize(scope[attrs.ngModel]);
+        }
+    };
+});
