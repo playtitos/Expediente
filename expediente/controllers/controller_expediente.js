1abaae5bd56ca73f3cce0138065b60557fd3749f
💪🏻 Luis Gallegos 🏋🏻‍♂️ <lgallegos@ohksystems.com>
2018-06-21 16:33:50 -0500

    se agregan validaciones

--- expediente/controllers/controller_expediente.js
+++ expediente/controllers/controller_expediente.js
@@ -10,10 +10,20 @@ secc_expediente.controller("cExpediente", function ($scope) {
 
     var lista_errores = {
         'curp': 'El curp debe contener 4 letras, 6 dígitos, 6 letras y 2 dígitos',
-        'nombre': 'json/getMetricaDispositivos',
+        'curp_vacio': 'Es necesario capturar el CURP',
+        'nombre': 'Es necesario capturar el nombre',
+        'apellido': 'Es necesario capturar el primer apellido',
+        'fecha': 'Es necesario capturar la fecha de nacimiento',
+        'sexo': 'Es necesario capturar el sexo',
+        'nacionalidad': 'Es necesario capturar la nacionalidad',
+        'estado_nacimiento': 'Es necesario capturar el estado de nacimiento',
+        'estado_residencia': 'Es necesario capturar el estado de residencia',
+        'municipio': 'Es necesario capturar el municipio',
+        'localidad': 'Es necesario capturar la localidad'
     };
 
     $scope.init_expediente = function () {
+        $scope.borraErrores();
         $scope.peticionGet($scope.urlGeneral + 'commons/src/listado.json', $scope.preparaListados);
         $scope.peticionGet($scope.urlGeneral + 'commons/src/nacionalidad.json', $scope.exitoNacionalidad);
         $scope.dis_municipio = true;
@@ -27,7 +37,7 @@ secc_expediente.controller("cExpediente", function ($scope) {
         let listaEstados = [];
         response.results.map((elemento) => {
             if (!listaEstados.includes(elemento.NOM_ENT)) {
-                var obj = { id_estado: elemento.CVE_ENT, nombre_estado: elemento.NOM_ENT.toUpperCase() }
+                var obj = { id_estado: elemento.CVE_ENT, nombre_estado: elemento.NOM_ENT.toUpperCase() };
                 $scope.estados.push(obj);
                 listaEstados.push(elemento.NOM_ENT);
             }
@@ -45,19 +55,13 @@ secc_expediente.controller("cExpediente", function ($scope) {
     };
 
     $scope.valida_campo = function (campo, valor) {
-        var validacion = true;
-        switch (campo) {
-            case 'curp':
-                //validacion = $scope.validaRFC(valor);
-                validacion = $scope.validaCURP(valor);
-                break;
-            default:
-                break;
-        }
-        (!validacion) ? ($scope.muestra_alertas('error', lista_errores[campo])) : ($scope.muestra_alertas('', ''));
+        $scope.borraErrores();
+        let validacion = $scope.validaCURP(valor);
+        (!validacion) ? ($scope.muestra_alertas('error', lista_errores[campo]), $scope.error_curp = 'error') : ('');
     };
 
     $scope.filtra_municipio = function (estado) {
+        $scope.borraErrores();
         $scope.municipios = [];
         let listaMunicipios = [];
         $scope.listadoCompleto.map((elemento) => {
@@ -73,6 +77,7 @@ secc_expediente.controller("cExpediente", function ($scope) {
     };
 
     $scope.filtraLocalidad = function (municipio) {
+        $scope.borraErrores();
         $scope.localidades = [];
         let listaLocalidades = [];
         $scope.listadoCompleto.map((elemento) => {
@@ -88,7 +93,57 @@ secc_expediente.controller("cExpediente", function ($scope) {
     };
 
     $scope.nac_cambio = function (nac) {
+        $scope.borraErrores();
         $scope.estado_nacimiento = '';
         (nac == 'MEX') ? $scope.dis_est_nac = false : $scope.dis_est_nac = true;
+    };
+
+    $scope.guardarRegistro = function () {
+        $scope.borraErrores();
+        if ($scope.validaDato($scope.curp) === '') {
+            $scope.muestra_alertas('error', lista_errores['curp_vacio']);
+            $scope.error_curp = 'error';
+        } else if ($scope.validaDato($scope.apprimero) === '') {
+            $scope.muestra_alertas('error', lista_errores['apellido']);
+            $scope.error_apprimero = 'error';
+        } else if ($scope.validaDato($scope.nombre) === '') {
+            $scope.muestra_alertas('error', lista_errores['nombre']);
+            $scope.error_nombre = 'error';
+        } else if ($scope.validaDato($scope.fecnac) === '') {
+            $scope.muestra_alertas('error', lista_errores['fecha']);
+            $scope.error_fecnac = 'error';
+        } else if ($scope.validaDato($scope.sexo) === '') {
+            $scope.muestra_alertas('error', lista_errores['sexo']);
+            $scope.error_sexo = 'error';
+        } else if ($scope.validaDato($scope.nacionalidad) === '') {
+            $scope.muestra_alertas('error', lista_errores['nacionalidad']);
+            $scope.error_nacionalidad = 'error';
+        } else if ($scope.nacionalidad === 'MEX' && $scope.validaDato($scope.estado_nacimiento) === '') {
+            $scope.muestra_alertas('error', lista_errores['estado_nacimiento']);
+            $scope.error_estado_nacimiento = 'error';
+        } else if ($scope.validaDato($scope.estado_residencia) === '') {
+            $scope.muestra_alertas('error', lista_errores['estado_residencia']);
+            $scope.error_estado_residencia = 'error';
+        } else if ($scope.validaDato($scope.municipio) === '') {
+            $scope.muestra_alertas('error', lista_errores['municipio']);
+            $scope.error_municipio = 'error';
+        } else if ($scope.validaDato($scope.localidad) === '') {
+            $scope.muestra_alertas('error', lista_errores['localidad']);
+            $scope.error_localidad = 'error';
+        }
+    };
+
+    $scope.borraErrores = function () {
+        $scope.muestra_alertas('', '');
+        $scope.error_curp = '';
+        $scope.error_apprimero = '';
+        $scope.error_nombre = '';
+        $scope.error_fecnac = '';
+        $scope.error_sexo = '';
+        $scope.error_nacionalidad = '';
+        $scope.error_estado_nacimiento = '';
+        $scope.error_estado_residencia = '';
+        $scope.error_municipio = '';
+        $scope.error_localidad = '';
     };
 });
--- expediente/directives/directive_expediente.js
+++
@@ -1,9 +0,0 @@
-secc_expediente.directive('dirTablaExpediente', [function () {
-    return {
-        restrict: 'E',
-        transclude: true,
-        replace: false,
-        scope: true,
-        templateUrl: '../../expediente/views/tabla_expediente.html'
-    };
-}]);
